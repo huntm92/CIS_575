29b009e997e6e192edf0b0b1f992f6b31b33f2c7
Mitch Hunt <Mitch.Hunt@nike.com>
2021-10-14 00:18:49 -0700

    Update Beer.py

--- CODE/Beer.py
+++ CODE/Beer.py
@@ -6,7 +6,7 @@ df_cols = df = pd.read_csv('/Users/MHunt5/Google Drive/School/CSU/STAA 556/Beer/
 cols = list(df_cols[0])
 
 # Read in Data.  This takes a while
-df = pd.read_csv('/Users/MHunt5/Google Drive/School/CSU/STAA 556/Beer/SNAP-Ratebeer.txt', sep = '[:]', header = None, engine = "python", usecols = [1])
+df = pd.read_csv('/Users/MHunt5/Google Drive/School/CSU/STAA 556/Beer/SNAP-Ratebeer.txt', sep = '(?:^\S*)', header = None, engine = "python", usecols = [1])
 df_t = pd.DataFrame(np.reshape(df.values,(int(df.shape[0] / 13),13)), columns = cols)
 df_t.columns = df_t.columns.str.replace("/","_")
 
